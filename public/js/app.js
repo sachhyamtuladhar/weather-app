0ca42dcbf1d23fccc365cda174d428a316a60474
sachhyam <tsachhyam@gmail.com>
2020-06-05 18:32:35 +0545

    Complete the weather app

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,21 @@
-fetch('/weather?address=7h7.gygy').then((response)=>{
-    response.json().then(data=>{
-        if (data.error){
-            console.log(data.error);
-            return
-        }
-        console.log(data.location);
-        console.log(data.forecastData);
+const weatherForm = document.querySelector('#weatherForm');
+const searchText = document.querySelector('#searchText');
+const forecast = document.querySelector('#forecast');
+const loc = document.querySelector('#location');
+
+weatherForm.addEventListener('submit', e=>{
+    e.preventDefault();
+    loc.textContent =  'Loading...';
+    forecast.textContent =  '';
+
+    fetch(`/weather?address=${searchText.value}`).then((response)=>{
+        response.json().then(data=>{
+            if (data.error){
+                loc.textContent=(data.error);
+                return
+            }
+            loc.textContent =  data.location;
+            forecast.textContent =  data.forecastData;
+        })
     })
 })
